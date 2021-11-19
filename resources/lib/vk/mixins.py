d9a2bd0c0a9048c31752ff2a20d5f28d9d474bee
Troikano <tdcp87@gmail.com>
2021-11-19 22:45:17 +0000

    Fixed new login flow

--- resources/lib/vk/mixins.py
+++ resources/lib/vk/mixins.py
@@ -130,10 +130,22 @@ class AuthMixin(object):
             'pass': self.user_password,
         }
         response = self.auth_session.post(self.AUTHORIZE_URL, auth_data)
-        login_form_data['ip_h']=re.compile(r'"ip_h" value="([^"]+)"').findall(response.text)[0]
-        login_form_data['lg_h']=re.compile(r'"lg_h" value="([^"]+)"').findall(response.text)[0]
-        login_form_data['lg_domain_h']=re.compile(r'"lg_domain_h" value="([^"]+)"').findall(response.text)[0]
-        login_form_data['to']=re.compile(r'"to" value="([^"]+)"').findall(response.text)[0]
+        try:
+            login_form_data['ip_h']=re.compile(r'"ip_h" value="([^"]+)"').findall(response.text)[0]
+        except:
+            pass
+        try:
+            login_form_data['lg_h']=re.compile(r'"lg_h" value="([^"]+)"').findall(response.text)[0]
+        except:
+            pass
+        try:
+            login_form_data['lg_domain_h']=re.compile(r'"lg_domain_h" value="([^"]+)"').findall(response.text)[0]
+        except:
+            pass
+        try:
+            login_form_data['to']=re.compile(r'"to" value="([^"]+)"').findall(response.text)[0]
+        except:
+            pass
         form_action = get_form_action(response.text)
         headers = { 
             'Host':'login.vk.com', 
